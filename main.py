fee5dddb3b43a908cd0f6c564963ec65bfa5bfa6
unoycerodigital <oscar.munera@unoycerodigital.com.co>
2023-05-23 14:42:56 -0500

    Cambio en paremetros de consulta

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get("/")
 async def root():
@@ -10,3 +11,8 @@ async def root():
 @app.get("/saludo")
 async def saludo():
     return {"message": "saludo en otra pagina"}
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
