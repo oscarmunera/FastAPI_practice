4c94aef0e688f4e44fdd009a0269cee60e424b20
unoycerodigital <oscar.munera@unoycerodigital.com.co>
2023-05-25 18:45:16 -0500

    Creación de rura put para terminar CRUD

--- api_practica/api_prueba.py
+++ api_practica/api_prueba.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class Producto(BaseModel):
     id: Optional[str]
     nombre: str
-    precio_compras: float
+    precio_compra: float
     precio_venta: float
     proveedor: str
 
@@ -44,4 +44,15 @@ def eliminar_producto(producto_id: str):
             productos.remove(producto)
             return {'mensaje': 'Producto eliminado'}
     raise HTTPException(status_code=404,detail='id de producto no encontrado, no se puede eliminar')
+
+@app.put('/productos/{producto_id}')
+def actualizar_producto(producto_id: str, producto: Producto):
+    for i, producto_encontrado in enumerate(productos):
+        if producto_encontrado.id == producto_id:
+            productos[i].nombre = producto.nombre
+            productos[i].precio_compra = producto.precio_compra
+            productos[i].precio_venta = producto.precio_venta
+            productos[i].proveedor = producto.proveedor
+            return productos[i]
+    raise HTTPException(status_code=404, detail='id de producto no encontrado, modificar')
 
